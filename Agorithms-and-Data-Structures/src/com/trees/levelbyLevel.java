e3d7dd9104f4a9171487c9fb9d14534d415f0e00
Shirley-Moraa <shirleyondieki@gmail.com>
2021-01-14 11:34:20 -0600

    new program

---
+++ Agorithms-and-Data-Structures/src/com/trees/levelbyLevel.java
@@ -0,0 +1,12 @@
+package com.trees;
+//Given a binary search tree, design an algorithm which creates a linked list of all the nodes at each depth 
+//(i e , if you have a tree with depth D, you’ll have D linked lists)
+
+public class levelbyLevel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      
+	}
+
+}
